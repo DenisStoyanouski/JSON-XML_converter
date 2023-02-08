38fc7331e3ae39e3e6cd15f6bfc1162eb68b1163
Denis Stoyanouski <vandenst@tut.by>
2023-02-08 23:09:49 +0300

    add print for method parseXmlToJson

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,6 +1,10 @@
 package converter;
 
+import java.lang.reflect.GenericArrayType;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,9 +13,11 @@ public class Converter {
     private static  String xml;
 
     private static final Pattern xmlString = Pattern.compile("<.+");
-    private static final Pattern openingTag = Pattern.compile("<[^<]+>");
+    private static final Pattern openingTag = Pattern.compile("<.+>");
     private static final Pattern closingTag = Pattern.compile("</[^<]+>");
-    private static final Pattern attribute = Pattern.compile(">.+<");
+    private static final Pattern element = Pattern.compile(">.+<");
+    private static final Pattern pTagName = Pattern.compile("<\\w+");
+    private static final Pattern pAttribute = Pattern.compile("\\w+\\s+=\\s+\"\\w+\"");
     private static final Pattern jsonString = Pattern.compile("\\s*?\\{.+");
     private static final Pattern pObjectKey = Pattern.compile("\\{\"\\w+\"\\s*?:");
     private static final Pattern pObjectValue = Pattern.compile(":.+}");
@@ -56,17 +62,39 @@ public class Converter {
     }
 
     private static void parseXmlToJson() {
-        Matcher open = openingTag.matcher(xml);
-        Matcher att = attribute.matcher(xml);
-        String tagName = null;
-        String attributeText = null;
+        Matcher mTagName = pTagName.matcher(xml);
+        Matcher mAttribute = pAttribute.matcher(xml);
+        Matcher ele = element.matcher(xml);
 
-        if (open.find()) {
-            tagName = open.group().replaceAll("(<|>|/>)", "\"");
+        String tagName = null;
+        List<String[]> attributes = new ArrayList<>();
+        String element = null;
+
+        if (mTagName.find()) {
+            tagName = mTagName.group().replaceAll("<", "");
         }
-        if (att.find()) {
-            attributeText = att.group().replaceAll("(<|>)", "\"");
+
+        while (mAttribute.find()) {
+            String attributeName = mAttribute.group().substring(0, mAttribute.group().indexOf("=")).trim();
+            String attributeValue = mAttribute.group().substring(mAttribute.group().indexOf("=") + 1).trim();
+            attributes.add(new String[] {"\"@" + attributeName + "\"" , attributeValue});
         }
-        System.out.printf("{%s:%s}", tagName, attributeText);
+        if (ele.find()) {
+            element = ele.group().replaceAll("<|>", "\"");
+        }
+        if (attributes.size() != 0) {
+            attributes.add(new String[] {"\"#" + tagName + "\"", element});
+        }
+
+        //Print JSON object
+        System.out.printf("{\"%s\" : ", tagName);
+        if (attributes.size() != 0) {
+            System.out.print("{");
+            attributes.forEach(x-> System.out.print("\n\t" + x[0] + " : " +  x[1]));
+            System.out.println("\n\t}");
+        } else {
+            System.out.print(element);
+        }
+        System.out.println("}");
     }
 }
